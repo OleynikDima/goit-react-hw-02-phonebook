11592bbac2d66d7e888f679eabefddc50c7cbff7
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-08-04 22:11:34 +0300

    add ContactLise & ContactInput  change func => class

--- phonebook/src/components/App.js
+++ phonebook/src/components/App.js
@@ -1,11 +1,90 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+// import PropsTypes from 'prop-types'
+import Section from './Section/Section'
+import Form from './Form/Form'
+import ContactsList from './Contacts/ContactsList'
+import ContactFilter from './Contacts/ContactFilter'
+
+const shortid = require('shortid');
+
+
+
 
 
 export default class App extends Component {
+
+    state = { 
+        contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+        filter:''
+    };
+//  функция записывает input contacts in filter
+    changeFilter = event => {
+        this.setState({filter:event.target.value})
+
+    }
+    updateInfo = () =>{      
+        const {filter, contacts} = this.state
+         return contacts.filter(contack => contack.name.toLowerCase().includes(filter.toLowerCase()))  
+    }
+//ф-я перезаписывает контакты 
+    handleCreateObjectContact = ({text,number}) =>{
+
+        const {contacts} =this.state
+        const filterName = contacts.map(contact => contact.name)
+// смотрим есть ли похожие contacts
+        if (filterName.includes(text)){
+            alert(text + ' is already in Contacts')
+        }else {
+
+//создаем обьект           
+            const newObj ={
+                id:shortid(),
+                name:text,
+                number:number,
+               }
+// перезаписываем stae.contacts  пополняем массив
+            this.setState((prevState)=>{
+                return {
+                    contacts:[...prevState.contacts,newObj],
+                }
+            })
+
+        }
+    }
+// Delete contact
+    onRemoveItem = itemId =>{
+        this.setState((prevState)=>{
+            return {
+                contacts: prevState.contacts.filter(contact => contact.id.indexOf(itemId) === -1)
+            }
+        })        
+    }
     render (){
+        const {filter} = this.state;
+        const visible = this.updateInfo();
         return (
-            <h1> Test  </h1>
+            <>
+            <Section title ="Phonebook">
+                <Form 
+                    onAddText={this.handleCreateObjectContact}/>
+            </Section>
+
+            <Section title ="Contacts">
+
+                <ContactFilter 
+                    onChangeFilter={this.changeFilter}
+                    value={filter}/>
+                   
+                <ContactsList 
+                    contacts ={visible}  
+                    onDelete={this.onRemoveItem}
+                    />
+            </Section>
+            </>
+
         )
     }
 }
